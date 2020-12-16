3476b4a4c5ef333858782d705cc76eee9eafc336
tangbearrrr <rh.sarawasee@gmail.com>
2020-12-16 20:45:29 +0700

    add click counter function

--- src/index.js
+++ src/index.js
@@ -1,17 +1,25 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDom from 'react-dom';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        
+        this.state = { count: 0 };
+    }
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    handleClick() {
+        this.setState(prevState => ({ count: prevState.count + 1}))
+    }
+
+    render() {
+        return (
+            <div> 
+                <h1>Count : {this.state.count}</h1> 
+                <button onClick={this.handleClick.bind(this)} >Click</button>
+            </div>
+        )
+    }
+}
+
+ReactDom.render(<App />, document.querySelector('#root'));
